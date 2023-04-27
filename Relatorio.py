fc300a03cbe5a6c7b2f9197db2283d55137d5d34
Henrique-Portes <portes.th@gmail.com>
2023-04-27 15:22:04 -0300

    Importando biblioteca Pandas

---
+++ Relatorio.py
@@ -0,0 +1,14 @@
+# Importar bibliotecas:
+import pandas as pd
+
+# importar base dados
+
+# Vizualizar base dados 
+
+# Faturamento por loja
+
+# Quantidade de produtos por loja
+
+# Ticket médio por produto em cada loja
+
+# Enviar um email com relatório 
